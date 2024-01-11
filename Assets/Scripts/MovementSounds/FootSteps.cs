d739269c6a0c0865b61d49dd310a284377117f1e
karastoychev <150927002+karastoychev@users.noreply.github.com>
2024-01-11 20:09:51 +0200

    Add footseps sound when player is walking (#9)
    * footsteps sound
    
    * feat: script adjustment to support controller joystick input
    
    ---------
    
    Co-authored-by: georgikarastoychev <georgi.karastoychev@accedia.com>
    Co-authored-by: Vanyo Ivanov <vanyo.ivanov@accedia.com>

---
+++ Assets/Scripts/MovementSounds/FootSteps.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.XR;
+
+public class FootSteps : MonoBehaviour
+{
+    public AudioClip footstepSound;
+    public float footstepInterval = 1f;
+    public float movementThreshold = 0.01f;
+
+    private AudioSource audioSource;
+    private bool isPlaying = false;
+    private float timer = 0.0f;
+    private float lastMovementTime = 0.0f;
+    private InputData _inputData;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        _inputData = GetComponent<InputData>();
+    }
+
+    void Update()
+    {
+        if (PlayerHasMoved())
+        {
+            if (!isPlaying || Time.time > lastMovementTime + footstepInterval)
+            {
+                timer = 0.0f;
+                isPlaying = true;
+                PlayFootstepSound();
+                lastMovementTime = Time.time;
+            }
+        }
+        else
+        {
+            if (isPlaying)
+            {
+                timer = 0.0f;
+                isPlaying = false;
+                audioSource.Stop();
+            }
+        }
+    }
+
+
+    bool PlayerHasMoved()
+    {
+        // Check if the left joystick has been moved
+        bool thumbstickMove;
+        Vector2 thumbstickValue;
+
+        if (_inputData._leftController.TryGetFeatureValue(CommonUsages.primary2DAxis, out thumbstickValue))
+        {
+            // Set a threshold value for joystick movement
+            float joystickThreshold = 0.2f;
+
+            // Check if the magnitude of the vector is greater than the threshold
+            thumbstickMove = thumbstickValue.magnitude > joystickThreshold;
+        }
+        else
+        {
+            thumbstickMove = false; // Default to false if unable to get the feature value
+        }
+
+        bool keyboardMove = Input.GetButton("Vertical") || Input.GetButton("Horizontal");
+        Debug.Log(keyboardMove || thumbstickMove);
+
+        return keyboardMove || thumbstickMove;
+    }
+
+    void PlayFootstepSound()
+    {
+        if (timer == 0.0f || Time.time > lastMovementTime + movementThreshold)
+        {
+            timer = footstepInterval;
+            audioSource.PlayOneShot(footstepSound);
+        }
+    }
+}
