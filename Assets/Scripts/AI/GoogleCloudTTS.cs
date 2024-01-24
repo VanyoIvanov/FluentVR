c380e5cf2d09970d7ac9c2aa8f755626e85505d7
karastoychev <karastoychev.georgi@gmail.com>
2024-01-24 20:28:53 +0200

    fix memory leak bug

--- Assets/Scripts/AI/GoogleCloudTTS.cs
+++ Assets/Scripts/AI/GoogleCloudTTS.cs
@@ -3,120 +3,122 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 using Newtonsoft.Json;
 
 public class GoogleCloudTTS : MonoBehaviour
 {
+    public static GoogleCloudTTS Instance;
 
-	public static GoogleCloudTTS Instance;
+    private string apiKey = "AIzaSyAA6D7PlQe3ERp4TzMQCgVJ_GXIfrcOqlw";
+    private const string postSynthURL = "https://texttospeech.googleapis.com/v1beta1/text:synthesize";
+    private const double sampleRate = 24000;
+    public double voicePitch;
+    public double voiceRate;
+    public string langCode = "es-ES";
+    public string voiceName = "es-ES-Neural2-A";
+    public SsmlVoiceGender voiceGender = SsmlVoiceGender.MALE;
 
-	public string apiKey;
-	private const string postSynthURL = "https://texttospeech.googleapis.com/v1beta1/text:synthesize";
-	private const string getVoicesURL = "https://texttospeech.googleapis.com/v1beta1/voices";
-	private const double sampleRate = 24000;
-	public double voicePitch;
-	public double voiceRate;
-	public string langCode = "es-ES";
-	public string voiceName = "es-ES-Neural2-A";
-	public SsmlVoiceGender voiceGender = SsmlVoiceGender.MALE;
+    public AudioSource audioSource;
 
-	public AudioSource audioSource;
+    private void SynthSuccess(string audioData)
+    {
+        var synthOutput = JsonConvert.DeserializeObject<SynthOutput>(audioData);
+        AudioClip loadClip = GetClipFromBase64(synthOutput.audioContent);
+        audioSource.clip = loadClip;
+        audioSource.time = 0.1f;
+        audioSource.Play();
+    }
 
-	private void SynthSuccess(string audioData)
-	{
-		var synthOutput = JsonConvert.DeserializeObject<SynthOutput>(audioData);
-		AudioClip loadClip = GetClipFromBase64(synthOutput.audioContent);
-		audioSource.clip = loadClip;
-		audioSource.time = 0.1f;
-		audioSource.Play();
-	}
+    public AudioClip GetClipFromBase64(string Base64data)
+    {
+        byte[] data = Convert.FromBase64String(Base64data);
+        float[] floatData = ConvertByteToFloat(data);
+        AudioClip newClip = AudioClip.Create("TTS_Clip", floatData.Length,
+            BitConverter.ToUInt16(data, 22),
+            BitConverter.ToInt32(data, 24), false);
+        newClip.SetData(floatData, 44);
+        return newClip;
+    }
 
-	public AudioClip GetClipFromBase64(string Base64data)
-	{
-		byte[] data = Convert.FromBase64String(Base64data);
-		float[] floatData = ConvertByteToFloat(data);
-		AudioClip newClip = AudioClip.Create("TTS_Clip", floatData.Length,
-			BitConverter.ToUInt16(data, 22),
-			BitConverter.ToInt32(data, 24), false);
-		newClip.SetData(floatData, 44);
-		return newClip;
-	}
+    private float[] ConvertByteToFloat(byte[] array)
+    {
+        float[] floatArr = new float[(array.Length / 2) - 22];
 
-	private float[] ConvertByteToFloat(byte[] array)
-	{
-		float[] floatArr = new float[(array.Length / 2) - 22];
+        for (int i = 0; i < floatArr.Length; i++)
+        {
+            floatArr[i] = ((float)BitConverter.ToInt16(array, i * 2)) / 32768.0f;
+        }
 
-		for (int i = 0; i < floatArr.Length; i++)
-		{
-			floatArr[i] = ((float)BitConverter.ToInt16(array, i * 2)) / 32768.0f;
-		}
+        return floatArr;
+    }
 
-		return floatArr;
-	}
+    public void Synthesize(string textToSynth)
+    {
+        StartCoroutine(RequestSynth(textToSynth));
+    }
 
-	public void Synthesize(string textToSynth)
-	{
-		StartCoroutine(RequestSynth(textToSynth));
-	}
+    private IEnumerator RequestSynth(string textToSynth)
+{
+    SynthObj newRequest = new SynthObj();
+    newRequest.input = new SynthInput() { text = textToSynth };
+    newRequest.audioConfig = GetAudioConfig();
+    newRequest.voice = GetVoiceConfig();
 
-	IEnumerator RequestSynth(string textToSynth)
-	{
-		SynthObj newRequest = new SynthObj();
-		newRequest.input = new SynthInput() { text = textToSynth };
-		newRequest.audioConfig = GetAudioConfig();
-		newRequest.voice = GetVoiceConfig();
+    string requestJson = JsonConvert.SerializeObject(newRequest);
+    Debug.Log("Request JSON: " + requestJson);  // Log the request JSON for debugging
+    byte[] bytes = Encoding.UTF8.GetBytes(requestJson);
 
-		// Access AudioSource dynamically
-		AudioSource audioSource = GetComponent<AudioSource>();
+    using (UnityWebRequest www = new UnityWebRequest(postSynthURL + "?key=" + apiKey, "POST"))
+    {
+        UploadHandlerRaw uploadHandler = new UploadHandlerRaw(bytes);
+        uploadHandler.contentType = "application/json";
+        www.uploadHandler = uploadHandler;
 
-		string requestJson = JsonConvert.SerializeObject(newRequest);
-		Debug.Log("Request JSON: " + requestJson);  // Log the request JSON for debugging
-		byte[] bytes = Encoding.UTF8.GetBytes(requestJson);
-		string url = postSynthURL + "?key=" + apiKey;
-		var headers = new Dictionary<string, string>();
-		headers.Add("Content-Type", "application/json");
-		WWW www = new WWW(postSynthURL + "?key=" + apiKey, bytes, headers);
+        www.downloadHandler = new DownloadHandlerBuffer();
 
-		yield return www;
+        yield return www.SendWebRequest();
 
-		if (www.error != null)
-		{
-			Debug.Log("Error: " + www.error);
-			Debug.Log("Response: " + www.text);  // Log the response for debugging
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log("Error: " + www.error);
+            Debug.Log("Response: " + www.downloadHandler.text);
 
-			// Handle the specific error in the response
-			if (!string.IsNullOrEmpty(www.text) && www.text.Contains("error"))
-			{
-				// Log additional details about the error
-				Debug.LogError("Error in API response: " + www.text);
-			}
-		}
-		else
-		{
-			SynthSuccess(www.text);
-		}
-	}
+            // Handle the specific error in the response
+            if (!string.IsNullOrEmpty(www.downloadHandler.text) && www.downloadHandler.text.Contains("error"))
+            {
+                // Log additional details about the error
+                Debug.LogError("Error in API response: " + www.downloadHandler.text);
+            }
+        }
+        else
+        {
+            SynthSuccess(www.downloadHandler.text);
+        }
+    }
+}
 
-	AudioConfig GetAudioConfig()
-	{
-		return new AudioConfig()
-		{
-			audioEncoding = AudioEncoding.LINEAR16,
-			pitch = voicePitch,
-			sampleRateHertz = sampleRate,
-			speakingRate = voiceRate,
-			volumeGainDb = 0.0
-		};
-	}
-	VoiceConfig GetVoiceConfig()
-	{
-		return new VoiceConfig()
-		{
-			languageCode = langCode,
-			name = voiceName,
-			ssmlGender = voiceGender
-		};
-	}
+    private AudioConfig GetAudioConfig()
+    {
+        return new AudioConfig()
+        {
+            audioEncoding = AudioEncoding.LINEAR16,
+            pitch = voicePitch,
+            sampleRateHertz = sampleRate,
+            speakingRate = voiceRate,
+            volumeGainDb = 0.0
+        };
+    }
+
+    private VoiceConfig GetVoiceConfig()
+    {
+        return new VoiceConfig()
+        {
+            languageCode = langCode,
+            name = voiceName,
+            ssmlGender = voiceGender
+        };
+    }
 }
 
 public enum SsmlVoiceGender
